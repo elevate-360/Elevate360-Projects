3c49d25041f6d977676c42858c053eab87b79c36
elevate-360 <elevate360.office.1@gmail.com>
2024-01-27 20:41:40 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,216 @@
+@extends('layout')
+
+@section('activeDb')
+    active
+@endsection
+
+@section('pageTitle')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"><strong>Transection details</strong></h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-hover table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Attempt No.</th>
+                                <th>Title</th>
+                                <th>Platform</th>
+                                <th>Technology</th>
+                                <th>Project Amount</th>
+                                <th>Contact Type</th>
+                                <th>Status</th>
+                                <th>Date-time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach ($data as $item)
+                                <tr>
+                                    <td>{{ ++$count }}</td>
+                                    <td>{{ $item->traTitle }}</td>
+                                    <td>{{ $item->traEntity }}</td>
+                                    <td>{{ $item->traAmount }}</td>
+                                    <td>
+                                        @if ($item->traType == 1)
+                                            <span class="badge bg-olive">Credit</span>
+                                        @else
+                                            <span class="badge bg-warning">Debit</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @switch($item->traMethod)
+                                            @case('0')
+                                                <span class="badge bg-info">Cash</span>
+                                            @break
+
+                                            @case('1')
+                                                <span class="badge bg-danger">Card</span>
+                                            @break
+
+                                            @case('2')
+                                                <span class="badge bg-success">UPI</span>
+                                            @break
+
+                                            @case('3')
+                                                <span class="badge bg-warning">Cheque</span>
+                                            @break
+                                        @endswitch
+                                    </td>
+                                    <td>
+                                        <div class="user-block">
+                                            <img class="img-circle img-bordered-sm" src="/dist/img/{{ $item->userProfile }}"
+                                                alt="user image">
+                                            <span class="username">
+                                                <a href="">{{ $item->userFirstName }}</a>
+                                            </span>
+                                            <span
+                                                class="description">{{ date('d M, Y - h:m a', strtotime($item->traDate)) }}</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach --}}
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Attempt No.</th>
+                                <th>Title</th>
+                                <th>Platform</th>
+                                <th>Technology</th>
+                                <th>Project Amount</th>
+                                <th>Contact Type</th>
+                                <th>Status</th>
+                                <th>Date-time</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Final Report --}}
+
+    {{-- <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"><strong>Final Report</strong></h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <td><strong>#</strong></td>
+                                <td><strong>Total Amount</strong></td>
+                                <td class="w-25">
+                                    <center><strong>-</strong></center>
+                                </td>
+                                <td>
+                                    <center><strong>-</strong></center>
+                                </td>
+                                <td><strong>{{ round($report['totalAmount'], 2) }}</strong>&nbsp&nbsp<i
+                                        class="fas fa-rupee-sign"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><strong>1.</strong></td>
+                                <td>Balance</td>
+                                <td>
+                                    <div class="progress progress-xs">
+                                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar"
+                                            style="width: {{ ($report['balance'] * 100) / $report['totalAmount'] }}%">
+                                        </div>
+                                    </div>
+                                </td>
+                                <td><span
+                                        class="badge bg-success">{{ round(($report['balance'] * 100) / $report['totalAmount'], 2) }}%</span>
+                                </td>
+                                <td><strong>{{ round($report['balance'], 2) }}</strong>&nbsp&nbsp<i
+                                        class="fas fa-rupee-sign"></i>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><strong>2.</strong></td>
+                                <td>Spent</td>
+                                <td>
+                                    <div class="progress progress-xs">
+                                        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar"
+                                            style="width: {{ ($report['spent'] * 100) / $report['totalAmount'] }}%">
+                                        </div>
+                                    </div>
+                                </td>
+                                <td><span
+                                        class="badge bg-danger">{{ round(($report['spent'] * 100) / $report['totalAmount'], 2) }}%</span>
+                                </td>
+                                <td><strong>{{ round($report['spent'], 2) }}</strong>&nbsp&nbsp<i
+                                        class="fas fa-rupee-sign"></i></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+    {{-- User wise Report --}}
+
+    {{-- <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"><strong>Detailed Report</strong></h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <td><strong>#</strong></td>
+                                <td><strong>User</strong></td>
+                                <td class="w-25"><strong>Amount Received</strong></td>
+                                <td><strong>Amount Spent</strong></td>
+                            </tr>
+                            @foreach ($userReport as $item)
+                                <tr>
+                                    <td><strong>{{ ++$userCount }}</strong></td>
+                                    <td>
+                                        <div class="user-block">
+                                            <img class="img-circle img-bordered-sm" src="/dist/img/{{ $item->userProfile }}"
+                                                alt="user image">
+                                            <span class="username">
+                                                <a href="">{{ $item->userFirstName . ' ' . $item->userLastName }}</a>
+                                            </span>
+                                            <span class="description">
+                                                @switch($item->userRole)
+                                                    @case('0')
+                                                        <span class="badge bg-info">Employee</span>
+                                                    @break
+
+                                                    @case('1')
+                                                        <span class="badge bg-success">CO-Founder</span>
+                                                    @break
+
+                                                    @case('2')
+                                                        <span class="badge bg-success">Company Profit</span>
+                                                    @break
+                                                @endswitch
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td>{{ $item->amountReceived }}</td>
+                                    <td>{{ $item->amountSpent }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+@endsection
---
+++ tables.sql
@@ -0,0 +1,51 @@
+CREATE TABLE tblUser (
+    userId int primary key AUTO_INCREMENT,
+    userFirstName varchar(128) not null,
+    userLastName varchar(128) not null,
+    userEmail varchar(512) not null,
+    userContactNumber char(10) not null,
+    userPassword varchar(128) not null,
+    userImage varchar(512) not null,
+    userRole char(1) not null,
+    isActive char(1) not null default '0'
+);
+CREATE TABLE tblBruteforce (
+    bfId int primary key AUTO_INCREMENT,
+    bfUserId int,
+    bfIP varchar(39) not null,
+    bfCount int not null default 0,
+    bfBrowserInfo varchar(128) not null,
+    bfOperatingSystem varchar(128) not null,
+    bfDeviceType varchar(128) not null,
+    bfTime datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    FOREIGN KEY (bfUserId) REFERENCES tblUser(userId)
+);
+CREATE TABLE tblMails (
+    mailId int primary key AUTO_INCREMENT,
+    mailBy int not null,
+    mailTo text not null,
+    mailFrom varchar(512) not null,
+    mailToName varchar(512) not null,
+    mailSubject varchar(128) not null,
+    mailContent text not null,
+    mailDate datetime not null default CURRENT_TIMESTAMP,
+    FOREIGN KEY (mailBy) REFERENCES tblUser(userId)
+);
+CREATE TABLE tblAttempts (
+    atpId int primary key AUTO_INCREMENT,
+    atpProjectName varchar(256) not null,
+    atpPlatform char(1) not null,
+    atpTechnology char(1) not null,
+    atpProjectAmount decimal(10, 2) not null,
+    atpContactType char(1) not null,
+    atpStatus char(1) not null,
+    atpDate datetime not null default CURRENT_TIMESTAMP,
+    atpUser int not null,
+    FOREIGN KEY (atpUser) REFERENCES tblUser(userId)
+);
+CREATE TABLE tblPlatforms (
+    pltId int primary key AUTO_INCREMENT,
+    pltName varchar(128) not null,
+    pltLogin varchar(512) not null,
+    pltPassword varchar(64) not null
+);
