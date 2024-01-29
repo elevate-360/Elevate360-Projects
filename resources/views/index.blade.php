cfb9ee004405137824db2747369eb49cc1ea177d
Jay Chauhan <152780847+elevate-360@users.noreply.github.com>
2024-01-29 17:09:39 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body table-responsive p-0">
-                    <table id="example1" class="table table-head-fixed text-nowrap">
+                    <table id="example1" class="table table-head-fixed">
                         <thead>
                             <tr>
                                 <th>Attempt No.</th>
