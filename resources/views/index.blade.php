24aa73f26f10b9ebb3fa5dcfbea36143f38d174b
Jay Chauhan <152780847+elevate-360@users.noreply.github.com>
2024-01-29 17:15:08 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,7 +169,7 @@
                 <div class="card-header">
                     <h3 class="card-title"><strong>Summary</strong></h3>
                 </div>
-                <div class="card-body table-responsive">
+                <div class="card-body table-responsive p0">
                     <table class="table table-hover table-bordered table-striped table-head-fixed text-nowrap" id="example2">
                         <tbody>
                             <tr>
