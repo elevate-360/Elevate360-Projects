efdfa151f78a1235974af792e3f9878ac10fdb09
elevate-360 <elevate360.office.1@gmail.com>
2024-02-01 21:24:24 +0530

    bug fixed

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -24,21 +24,21 @@
                     <div class="form-group">
                         <label for="exampleInputEmail1">Title</label>
                         <input type="text" class="form-control" id="exampleInputEmail1" name="atpTitle"
-                            placeholder="Enter project title">
+                            placeholder="Enter project title" required>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail2">Link</label>
                         <input type="text" class="form-control" id="exampleInputEmail2" name="atpLink"
-                            placeholder="Enter project link">
+                            placeholder="Enter project link" required>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Descreption</label>
-                        <textarea name="atpDesc" class="form-control" rows="4" placeholder="Enter project descreption"></textarea>
+                        <textarea name="atpDesc" class="form-control" rows="4" placeholder="Enter project descreption" required></textarea>
                     </div>
                     <div class="form-group">
                         <label for="amount">Amount</label>
                         <input type="text" class="form-control" id="amount" name="atpAmount"
-                            placeholder="Enter project amount" />
+                            placeholder="Enter project amount" required/>
                     </div>
                     <div class="form-group">
                         <label for="type">Transection Type</label><br>
@@ -46,7 +46,7 @@
                             <tr>
                                 <td>
                                     <div class="icheck-primary d-inline">
-                                        <input type="radio" name="atpPlatform" value="1" id="checkboxPrimary1">
+                                        <input type="radio" name="atpPlatform" value="1" id="checkboxPrimary1" required>
                                         <label for="checkboxPrimary1">Freelancers</label>
                                     </div>
                                 </td>
@@ -54,7 +54,7 @@
                             <tr>
                                 <td>
                                     <div class="icheck-primary d-inline">
-                                        <input type="radio" name="atpPlatform" value="2" id="checkboxPrimary2">
+                                        <input type="radio" name="atpPlatform" value="2" id="checkboxPrimary2" required>
                                         <label for="checkboxPrimary2">Upwork</label>
                                     </div>
                                 </td>
@@ -62,7 +62,7 @@
                             <tr>
                                 <td>
                                     <div class="icheck-primary d-inline">
-                                        <input type="radio" name="atpPlatform" value="3" id="checkboxPrimary3">
+                                        <input type="radio" name="atpPlatform" value="3" id="checkboxPrimary3" required>
                                         <label for="checkboxPrimary3">Linkedin</label>
                                     </div>
                                 </td>
