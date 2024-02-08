a097af43daf5a6457b4345f243a52530f485b20a
Jay Chauhan <152780847+elevate-360@users.noreply.github.com>
2024-02-08 20:13:03 +0530

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -221,7 +221,7 @@
         </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2023 <a href="https://dj-jay.in">Jay Chauhan</a>.</strong>
+            <strong>Copyright &copy; 2023 <a href="https://dj-jay.in" target="_blank">Jay Chauhan</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
                 <b>Version</b> 3.2.0
